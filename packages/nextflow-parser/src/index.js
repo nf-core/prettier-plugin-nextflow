f4001b4ad0eae284be63af70381276fe1066c660
Edmund Miller <edmund.a.miller@protonmail.com>
2022-03-21 12:13:19 -0500

    refactor(parser): Split lexer out from index

--- packages/nextflow-parser/src/index.js
+++ packages/nextflow-parser/src/index.js
@@ -1,41 +1,4 @@
-const chevrotain = require("chevrotain");
-
-const createToken = chevrotain.createToken
-
-// using createToken API
-
-const ProcessStart = createToken({ name: "process", pattern: /process\s+\w+\s+{\s+/ })
-
-const ProcessEnd = createToken({ name: "process", pattern: /\}/ })
-
-const Input = createToken({ name: "input", pattern: /input:/ })
-
-const Output = createToken({ name: "output", pattern: /output:/ })
-
-const When = createToken({ name: "when", pattern: /when:/ })
-
-const Script = createToken({ name: "script", pattern: /script:/ })
-
-const WhiteSpace = createToken({
-  name: "WhiteSpace",
-  pattern: /\s+/,
-  group: chevrotain.Lexer.SKIPPED
-})
-
-// note we are placing WhiteSpace first as it is very common thus it will speed up the lexer.
-let allTokens = [
-  WhiteSpace,
-  // "keywords" appear before the Identifier
-  ProcessStart,
-  ProcessEnd,
-  // The Identifier must appear after the keywords because all keywords are valid identifiers.
-  Input,
-  Output,
-  When,
-  Script
-]
-const Lexer = chevrotain.Lexer;
-let SelectLexer = new Lexer(allTokens)
+const NextflowLexer = require("./lexer");
 
 let inputText = `process name {
 
@@ -50,5 +13,5 @@ let inputText = `process name {
 
 }
 `
-let lexingResult = SelectLexer.tokenize(inputText)
+let lexingResult = NextflowLexer.tokenize(inputText)
 console.log(lexingResult);
